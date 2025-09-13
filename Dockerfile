a0a63e922081fbc6b40c3dcf5226e80270a71c9f
Micorksen <hello@micorksen.eu>
2025-09-13 13:05:16 +0200

    chore: Multiple optimizations

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM boky/postfix
+FROM boky/postfix:4.4.0-alpine
 
 LABEL maintainer="Groupe Speed Cloud <contact@groupe-speed.cloud>"
 LABEL org.opencontainers.image.source="https://github.com/speed-cloud/chatwoot-postfix"
@@ -7,8 +7,6 @@ LABEL org.opencontainers.image.description="A simple and \"GitOps-y\" way to use
 COPY bin/import-mail-to-rails /usr/local/bin/
 COPY bin/setup.sh /docker-init.db/
 COPY conf/dh2048.pem /etc/postfix/
-COPY conf/transport /etc/postfix/
-COPY conf/virtual_alias /etc/postfix/
 
 ENV ALLOW_EMPTY_SENDER_DOMAINS=true
 ENV OPENDKIM_Domain=*
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
-lightblue="$(printf '\033[38;5;147m')"
-red="$(printf '\033[91m')"
-reset="$(printf '\033[0m')"
+set -euo pipefail
 
-error() {
-  printf "${reset}‣ ${red}ERROR ${reset} "
-  echo -e "$@${reset}"
-}
+LIGHTBLUE="$(printf '\033[38;5;147m')"
+RED="$(printf '\033[91m')"
+RESET="$(printf '\033[0m')"
 
-info() {
-  printf "${reset}‣ ${lightblue}INFO ${reset} "
-  echo -e "$@${reset}"
-}
+error() { printf "‣ ${RED}ERROR${RESET} %b\n" "$@"; }
+info() { printf "‣ ${LIGHTBLUE}INFO${RESET} %b\n" "$@"; }
+
+# --------------------------------------------------
 
 if [ -z "$MAILER_INBOUND_EMAIL_DOMAIN" ]; then
   error "Please define the MAILER_INBOUND_EMAIL_DOMAIN environment variable before starting this container."
@@ -28,30 +25,46 @@ if [ -z "$RAILS_INBOUND_EMAIL_PASSWORD" ]; then
   exit 1
 fi
 
-info "Making import-mail-to-rails executable."
+# --------------------------------------------------
+
+info "Setting up import-mail-to-rails requirements."
+
 chmod +x /usr/local/bin/import-mail-to-rails
+useradd -m rails
+
+cat > /etc/postfix/import-mail-to-rails.cf <<EOF
+RAILS_HOST="${RAILS_HOST}"
+RAILS_INBOUND_EMAIL_PASSWORD="${RAILS_INBOUND_EMAIL_PASSWORD}"
+EOF
+
+install -m 400 -o rails -g rails /etc/postfix/import-mail-to-rails.cf /etc/postfix/import-mail-to-rails.cf
+
+# --------------------------------------------------
 
 info "Setting up Postfix requirements."
-sed -i "s/{MAILER_INBOUND_EMAIL_DOMAIN}/$MAILER_INBOUND_EMAIL_DOMAIN/" /etc/postfix/{transport,virtual_alias}
-postmap /etc/postfix/{transport,virtual_alias}
-postconf -M "rails/unix=rails unix - n n - - pipe flags=Xhq user=nobody argv=/usr/local/bin/import-mail-to-rails"
+
+echo "${MAILER_INBOUND_EMAIL_DOMAIN} rails:" > /etc/postfix/transport
+postmap /etc/postfix/transport
+postconf -M "rails/unix=rails unix - n n - - pipe flags=Xhq user=rails argv=/usr/local/bin/import-mail-to-rails"
+
+# --------------------------------------------------
 
 info "Configuring Postfix for Chatwoot."
-postconf -e "smtpd_client_restrictions=permit_mynetworks,permit_sasl_authenticated,reject_unauth_destination"
-postconf -e "smtpd_sender_restrictions=permit_mynetworks,reject_unauth_destination"
-postconf -e "transport_maps=lmdb:/etc/postfix/transport"
-postconf -e "virtual_alias_maps=lmdb:/etc/postfix/virtual_alias"
+
+postconf -e "smtpd_client_restrictions=permit_mynetworks,permit_sasl_authenticated,reject_unauth_destination" \
+  "smtpd_sender_restrictions=permit_mynetworks,reject_unauth_destination" \
+  "transport_maps=lmdb:/etc/postfix/transport"
+
+# --------------------------------------------------
 
 info "Securing Postfix for inbound emails."
-postconf -e "smtp_tls_auth_only=yes"
-postconf -e "smtp_tls_security_level=may"
-postconf -e "smtp_tls_mandatory_protocols=>=TLSv1.2"
-postconf -e "smtp_tls_protocols=>=TLSv1.2"
-
-postconf -e "smtpd_tls_dh1024_param_file=/etc/postfix/dh2048.pem"
-postconf -e "smtpd_tls_security_level=may"
-postconf -e "smtpd_tls_mandatory_protocols=>=TLSv1.2"
-postconf -e "smtpd_tls_protocols=>=TLSv1.2"
-
-postconf -e "tls_eecdh_auto_curves=X25519 prime256v1 secp384r1"
-postconf -e "tls_medium_cipherlist=ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-CHACHA20-POLY1305"
+postconf -e "smtp_tls_security_level=may" \
+  "smtp_tls_mandatory_protocols=>=TLSv1.2" \
+  "smtp_tls_protocols=>=TLSv1.2" \
+  "smtpd_tls_auth_only=yes" \
+  "smtpd_tls_dh1024_param_file=/etc/postfix/dh2048.pem" \
+  "smtpd_tls_security_level=may" \
+  "smtpd_tls_mandatory_protocols=>=TLSv1.2" \
+  "smtpd_tls_protocols=>=TLSv1.2" \
+  "tls_eecdh_auto_curves=X25519 prime256v1 secp384r1" \
+  "tls_medium_cipherlist=ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-CHACHA20-POLY1305"
